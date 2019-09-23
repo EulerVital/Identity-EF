32effbaf7ef25ed6fd2854c788a77aebb7b59829
Euler Vital <eulervital@outlook.com>
2019-09-23 16:40:58 -0300

    Adicionar arquivos de projeto.

---
+++ Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Data
+{
+    /// <summary>
+    /// A classe de contexto agora herda da IdentiyDbContext, pois já cria todas as configurações de login
+    /// </summary>
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
